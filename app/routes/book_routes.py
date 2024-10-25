1eee5c5cc95246669d6067a09490f71506d5a093
Charday Neal <charday.neal@gmail.com>
2024-10-25 09:26:59 -0700

    create database and connect to flask app

--- app/routes/book_routes.py
+++ app/routes/book_routes.py
@@ -1,22 +1,46 @@
-from flask import Blueprint, abort, make_response
-from ..models.book import books
+from flask import Blueprint, abort, make_response, request
+from app.models.book import Book
+from ..db import db
+# from ..models.book import books
 
 books_bp = Blueprint("books_bp", __name__, url_prefix="/books")
 
+@books_bp.post("")
+def create_book():
+    request_body = request.get_json()
+    title = request_body['title']
+    description = request_body['description']
+
+    new_book = Book(title=title, description=description)
+    db.session.add(new_book)
+    db.session.commit()
+
+    response = {
+        "id": new_book.id,
+        "title": new_book.title,
+        "description": new_book.description
+    }
+    return response, 201
+
 @books_bp.get("")
 def get_all_books():
+    query = db.select(Book).order_by(Book.id)
+    books = db.session.scalars(query)
+
     books_response = []
     for book in books:
         books_response.append(
             {
-                "id": book.id,
-                "title": book.title,
-                "description": book.description
+                'id': book.id,
+                'title': book.title,
+                'description': book.description
             }
         )
-    
     return books_response
-
+    
+    
+    
+'''
 @books_bp.get("/<book_id>")
 def get_book_by_id(book_id):
     book = validate_book(book_id)
@@ -26,7 +50,6 @@ def get_book_by_id(book_id):
         "title" : book.title,
         "book_description" : book.description  
     }
-
 
 
 def validate_book(book_id):
@@ -42,3 +65,5 @@ def validate_book(book_id):
     
     response = {"message": f"Book {book_id} not found"}
     abort(make_response(response, 404))
+
+'''
