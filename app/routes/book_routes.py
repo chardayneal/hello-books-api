d79ca7637550e4bb10f652954341bd5d8af88a1b
Charday Neal <charday.neal@gmail.com>
2024-10-28 17:26:42 -0700

    update books endpoint to accept put and delete requests

--- app/routes/book_routes.py
+++ app/routes/book_routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, abort, make_response, request
+from flask import Blueprint, abort, make_response, request, Response
 from app.models.book import Book
 from ..db import db
 # from ..models.book import books
@@ -15,42 +15,38 @@ def create_book():
     db.session.add(new_book)
     db.session.commit()
 
-    response = {
-        "id": new_book.id,
-        "title": new_book.title,
-        "description": new_book.description
-    }
-    return response, 201
+    return new_book.to_dict(), 201
 
 @books_bp.get("")
 def get_all_books():
     query = db.select(Book).order_by(Book.id)
     books = db.session.scalars(query)
 
-    books_response = []
-    for book in books:
-        books_response.append(
-            {
-                'id': book.id,
-                'title': book.title,
-                'description': book.description
-            }
-        )
-    return books_response
+    return [book.to_dict() for book in books]
+
     
-    
-    
-'''
 @books_bp.get("/<book_id>")
-def get_book_by_id(book_id):
+def get_one_book(book_id):
+    return validate_book(book_id).to_dict()
+
+@books_bp.put("/book_id")
+def update_one_book(book_id):
     book = validate_book(book_id)
 
-    return {
-        "id" : book.id,
-        "title" : book.title,
-        "book_description" : book.description  
-    }
+    request_body = request.get_json()
+    for key, value in request_body:
+        book[key] = value
+    db.session.commit()
 
+    return Response(status=204, mimetype="application/json")
+
+@books_bp.delete("/<book_id>")
+def delete_book(book_id):
+    book = validate_book(book_id)
+    db.session.delete(book)
+    db.session.commit()
+
+    return Response(status=204, mimetype="application/json")
 
 def validate_book(book_id):
     try:
@@ -59,11 +55,12 @@ def validate_book(book_id):
         response = {"message": f"book {book_id} invalid"}
         abort(make_response(response, 400))
 
-    for book in books:
-        if book_id == book.id:
-            return book
+    query = db.select(Book).where(Book.id == book_id)
+    book = db.session.scalar(query)
+
+    if book:
+        return book
     
     response = {"message": f"Book {book_id} not found"}
     abort(make_response(response, 404))
 
-'''
