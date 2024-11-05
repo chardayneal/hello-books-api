9443b86fc18f0f49c458bcee8db51b992fa3b36d
Charday Neal <charday.neal@gmail.com>
2024-11-04 18:31:44 -0800

    Establish relationship with author and book model using foreign key. Migrate changes.

--- app/models/author.py
+++ app/models/author.py
@@ -1,9 +1,13 @@
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 from app.db import db
+from typing import TYPE_CHECKING
+if TYPE_CHECKING:
+    from app.models.book import Book
 
 class Author(db.Model):
     id: Mapped[int] = mapped_column(autoincrement=True, primary_key=True)
     name: Mapped[str]
+    books: Mapped[list["Book"]] = relationship(back_populates="author")
 
     def to_dict(self):
         return {
--- app/models/book.py
+++ app/models/book.py
@@ -1,10 +1,16 @@
-from sqlalchemy.orm import Mapped, mapped_column
-from ..db import db
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy import ForeignKey
+from typing import Optional, TYPE_CHECKING
+if TYPE_CHECKING:
+    from app.models.author import Author
+from app.db import db
 
 class Book(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
     title: Mapped[str]
     description: Mapped[str]
+    author_id: Mapped[Optional[int]] = mapped_column(ForeignKey("author.id"))
+    author: Mapped[Optional["Author"]] = relationship(back_populates="books")
 
     @classmethod
     def from_dict(cls, new_dict):
